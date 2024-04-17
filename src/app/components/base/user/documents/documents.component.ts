455c4162a6bb48cc73a316a2c6dcf1fac3b9d1dd
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2024-04-17 10:05:43 +0200

    added doc

--- src/app/components/base/user/documents/documents.component.ts
+++ src/app/components/base/user/documents/documents.component.ts
@@ -1,21 +1,31 @@
 import {Component} from '@angular/core';
 import {UserService} from "../../../../services/user/user.service";
 import {Router} from "@angular/router";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {NavService} from "../../../../services/nav/nav.service";
+import {DocumentService} from "../../../../services/document/document.service";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-documents',
   standalone: true,
   imports: [
-    NgIf
+    NgIf,
+    NgForOf,
+    ReactiveFormsModule
   ],
   templateUrl: './documents.component.html'
 })
 export class DocumentsComponent {
   showModal = false;
+  addDocForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
 
-  constructor(private userService: UserService, private router: Router, private navService: NavService) {
+
+  constructor(private userService: UserService, private router: Router, private navService: NavService,
+              public documentService: DocumentService) {
     if (!this.userService.isUserLogged()) {
       this.router.navigate(['/auth/login']);
       return;
@@ -24,8 +34,22 @@ export class DocumentsComponent {
     this.navService.setCurrentPage('documents');
   }
 
-  toggleModal() {
+  async toggleModal(): Promise<void> {
     this.showModal = !this.showModal;
   }
 
+  async addDocument(): Promise<void> {
+    if (this.addDocForm.value.name === '' || this.addDocForm.value.description === '') {
+      return;
+    }
+    this.documentService.addDocument({id: this.documentService.getDocuments().length + 1, name: this.addDocForm.value.name, content: this.addDocForm.value.description});
+    this.addDocForm.reset();
+    await this.toggleModal();
+  }
+
+  async deleteDocument(id: number): Promise<void> {
+    this.documentService.removeDocument(this.documentService.getDocuments().find(doc => doc.id === id));
+  }
+
+  protected readonly document = document;
 }
---
+++ src/app/services/document/document.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {LocalService} from "../local/local.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+  private documentsList: any[] = [] // Array of documents
+
+  constructor(private localService: LocalService) {
+    if (!this.localService.getLocalStorage.getItem('documentsList')) {
+      this.documentsList.push({id: 1, name: 'Example1.pdf', content: 'Content of document 1'});
+      this.localService.getLocalStorage.setItem('documentsList', JSON.stringify(this.documentsList));
+      return;
+    }
+    if (this.localService.getLocalStorage.getItem('documentsList') != null) {
+      // @ts-ignore
+      const save: string = this.localService.getLocalStorage.getItem('documentsList');
+      this.documentsList = JSON.parse(save);
+    }
+  }
+
+  getDocuments() {
+    return this.documentsList;
+  }
+
+  addDocument(document: any) {
+    this.documentsList.push(document);
+    this.localService.getLocalStorage.setItem('documentsList', JSON.stringify(this.documentsList));
+  }
+
+  removeDocument(document: any) {
+    const index = this.documentsList.indexOf(document);
+    this.documentsList.splice(index, 1);
+    this.localService.getLocalStorage.setItem('documentsList', JSON.stringify(this.documentsList));
+  }
+}
